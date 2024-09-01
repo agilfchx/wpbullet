54167b349fa786013303fd7eb5d10c6524743540
agilfchx <agilfachrian2@gmail.com>
2024-09-01 15:36:03 +0700

    Added converter json to html

---
+++ core/converter.py
@@ -0,0 +1,64 @@
+import json
+import os
+
+def convert_json_to_html(json_filepath, output_filepath):
+    with open(json_filepath, 'r') as json_file:
+        data = json.load(json_file)
+
+    if not data:
+        return
+
+    report_name = os.path.splitext(os.path.basename(json_filepath))[0]
+
+    template_filepath = 'core/template.html'
+
+    try:
+        with open(template_filepath, 'r') as template_file:
+            template = template_file.read()
+    except FileNotFoundError:
+        print(f"Error: Template file not found at {template_filepath}")
+        return
+
+    if isinstance(data[0], dict):
+        headers = [header.upper() for header in data[0].keys()]
+    elif isinstance(data[0], list):
+        headers = [f"Column {i+1}".upper() for i in range(len(data[0]))]
+    else:
+        print("Error: Unexpected data format.")
+        return
+
+    table_headers = ''.join(f'<th>{header}</th>' for header in headers)
+
+    table_rows = ''
+    for item in data:
+        if isinstance(item, dict):
+            severity = item.get('severity', '').lower()
+            class_name = ''
+            if severity == 'low':
+                class_name = 'low'
+            elif severity == 'medium':
+                class_name = 'medium'
+            elif severity == 'high':
+                class_name = 'high'
+            elif severity == 'critical':
+                class_name = 'critical'
+            
+            row = ''.join(f'<td>{item.get(header.lower(), "")}</td>' for header in headers)
+            table_rows += f'<tr class="{class_name}">{row}</tr>'
+        elif isinstance(item, list):
+            row = ''.join(f'<td>{value}</td>' for value in item)
+            table_rows += f'<tr>{row}</tr>'
+        else:
+            row = ''
+            table_rows += f'<tr>{row}</tr>'
+
+    html_content = template.replace('{{ report_name }}', report_name)
+    html_content = html_content.replace('{{ table_headers }}', table_headers)
+    html_content = html_content.replace('{{ table_rows }}', table_rows)
+
+    try:
+        with open(output_filepath, 'w') as output_file:
+            output_file.write(html_content)
+    except IOError:
+        print(f"Error: Failed to write HTML file to {output_filepath}")
+        return
